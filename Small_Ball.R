0a04ccd0fbc7225dfd2f705c665890d1f1ed5028
elliotastern <stern.elliot@gmail.com>
2021-08-18 12:08:42 -0700

    Update Small_Ball.R

--- Small_Ball.R
+++ Small_Ball.R
@@ -21,16 +21,16 @@ ggplot(spurs, aes(x = Year, y = Adjusted.Defensive.Ratings, color = Rim.Protecto
   geom_point(size = 3, shape = 15) +
   ylab("Defensive Rating") +
   ylim(1, 30) +
-  geom_text(data = subset(spurs, Year == "1998-99"), vjust = -1, size = 5, aes(fontface=2)) +
-  geom_text(data = subset(spurs, Year == "1994-95"), vjust = -1, hjust = 0, size = 5, aes(fontface=2)) +
+  geom_text(data = subset(spurs, Year == "1998-99"), vjust = -1, size = 5, aes(fontface = 2)) +
+  geom_text(data = subset(spurs, Year == "1994-95"), vjust = -1, hjust = 0, size = 5, aes(fontface = 2)) +
   geom_text(data = subset(spurs, Year == "1996-97"), vjust = +1.5, size = 5, aes(fontface=2)) +
   theme(axis.text.x = element_text(face = "bold"),
         axis.text.y = element_text(angle = 45))
   
 ggplot(spurs, aes(x = Year, y = Adjusted.Defensive.Ratings, label = Duncan, color = Rim.Protector)) +
-  geom_text(data = subset(spurs, Year == "1998-99"), vjust = -1, size = 5, aes(fontface=2)) +
-  geom_text(data = subset(spurs, Year == "1994-95"), vjust = -1, hjust = 0, size = 5, aes(fontface=2)) +
-  geom_text(data = subset(spurs, Year == "1996-97"), vjust = +1.5, size = 5, aes(fontface=2)) +
+  geom_text(data = subset(spurs, Year == "1998-99"), vjust = -1, size = 5, aes(fontface = 2)) +
+  geom_text(data = subset(spurs, Year == "1994-95"), vjust = -1, hjust = 0, size = 5, aes(fontface = 2)) +
+  geom_text(data = subset(spurs, Year == "1996-97"), vjust = +1.5, size = 5, aes(fontface = 2)) +
   theme(axis.text.y = element_text(angle = 45),
         axis.text.x = element_text(face = "bold")) +
   ylim(1, 30) +
