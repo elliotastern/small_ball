36ccf037b3ec81df60001dcd6072061a8ca40bf8
elliotastern <stern.elliot@gmail.com>
2021-08-18 12:04:57 -0700

    Update Small_Ball_2.R

--- Small_Ball_2.R
+++ Small_Ball_2.R
@@ -1,9 +1,19 @@
-library(dplyr)
-library(ggplot2)
+##############################
+# 0 - LOAD PACKAGE   
+##############################
+library('tidyverse') #1.2.1
 
-# #Import Data
-nba <- read.csv("file:///C:/Users/stern/Downloads/nba_2014_15.csv", stringsAsFactors = FALSE)
+############################## 
+# 1 - LOAD DATA
+##############################
+nba <- read.csv("nba_2014_15.csv", stringsAsFactors = FALSE)
+nba16 <- read.csv("nba_2015_16.csv", stringsAsFactors = FALSE)
+nba17 <- read.csv("nba_2016_17.csv", stringsAsFactors = FALSE)
 
+
+############################## 
+# 2 - DATA CLEANING
+##############################
 # Clean Date column
 nba$DATE <- as.Date(nba$DATE, format = "%m/%d/%Y")
 
@@ -28,29 +38,11 @@ mean(cleveland$OEFF[cleveland$Mozgov == "Without Mozgov 2014-15"])
 lines <- cleveland %>%
   group_by(Mozgov) %>%
   summarise(mean_defense = mean(DEFF))
-  
-ggplot(cleveland, aes(x = DATE, y = DEFF, color = Mozgov)) +
-  geom_point() +
-  geom_boxplot(alpha = .4)  +
-  theme_minimal()
-
-
-ggplot(cleveland, aes(x = DATE, y = DEFF, color = Mozgov)) +
-  geom_point() +
-  geom_hline(yintercept=with_moz,    color = "turquoise",       lwd = 2)+
-  geom_hline(yintercept=without_moz, color = "red", lwd = 2)+
-  theme_minimal() +
-  geom_text(aes(label = "Average Without Mozgov",    y = with_moz   + 5, x = as.Date("2015-01-09", format = "%Y-%m-%d")- 30) , colour="red4", fontface = "bold" , angle=0, text=element_text(size=27)) +
-  geom_text(aes(label = "Average With Mozgov", y = without_moz- 5, x = as.Date("2015-01-09", format = "%Y-%m-%d")+ 55) , colour="turquoise4", fontface = "bold" , angle=0, text=element_text(size=27))
-
-
-
-# Import Data
-nba16 <- read.csv("file:///C:/Users/stern/Documents/nba_2015_16.csv", stringsAsFactors = FALSE)
 
 
 # Clean Date column
 nba16$DATE <- as.Date(nba16$DATE, format = "%m/%d/%Y")
+
 
 # Limit to Cleveland Games
 cleveland16 <- nba16 %>%
@@ -64,15 +56,6 @@ cleveland16$Mozgov <- "With Mozgov 2015-16"
 
 cleveland_combined <- rbind(cleveland[c("DATE", "DEFF", "Mozgov")]
                         , cleveland16[c("DATE", "DEFF", "Mozgov")])
-
-ggplot(cleveland_combined, aes(x = DATE, y = DEFF, color = Mozgov)) +
-  geom_point() +
-  geom_boxplot(alpha = .4)  +
-  theme_minimal()
-
-# Import Data
-nba17 <- read.csv("file:///C:/Users/stern/Documents/nba_2016_17.csv", stringsAsFactors = FALSE)
-
 
 # Clean Date column
 nba17$DATE <- as.Date(nba17$DATE, format = "%m/%d/%Y")
@@ -89,11 +72,16 @@ cleveland17$Mozgov <- "Without Mozgov 2016-17"
 cleveland_combined <- rbind(cleveland_combined[c("DATE", "DEFF", "Mozgov")]
                             , cleveland17[c("DATE", "DEFF", "Mozgov")])
 
-ggplot(cleveland_combined, aes(x = DATE, y = DEFF, color = Mozgov)) +
+############################## 
+# 3 - DATA VISUALIZATION
+############################## 
+ggplot(cleveland, aes(x = DATE, y = DEFF, color = Mozgov)) +
   geom_point() +
-  geom_boxplot(alpha = .4)  +
-  theme_minimal()
-
+  geom_hline(yintercept=with_moz,    color = "turquoise",       lwd = 2)+
+  geom_hline(yintercept=without_moz, color = "red", lwd = 2)+
+  theme_minimal() +
+  geom_text(aes(label = "Average Without Mozgov",    y = with_moz   + 5, x = as.Date("2015-01-09", format = "%Y-%m-%d")- 30) , colour="red4", fontface = "bold" , angle=0, text=element_text(size=27)) +
+  geom_text(aes(label = "Average With Mozgov", y = without_moz- 5, x = as.Date("2015-01-09", format = "%Y-%m-%d")+ 55) , colour="turquoise4", fontface = "bold" , angle=0, text=element_text(size=27))
 
 
 ggplot(cleveland_combined, aes(x = DATE, y = DEFF, color = Mozgov)) +
